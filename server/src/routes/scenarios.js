8c2343d30ae573a12cd1c7ed5792cc91f4f15dc4
salmaabdallah789 <salmaabdallah451@gmail.com>
2025-12-30 08:44:34 +0300

    asdasdasd

--- server/src/routes/scenarios.js
+++ server/src/routes/scenarios.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getScenarios, getScenarioById } from '../controllers/scenarioController.js';
-
+//test log
 const router = express.Router();
 
 router.get('/', getScenarios);
